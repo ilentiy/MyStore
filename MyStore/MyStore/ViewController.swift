441c1fab985e4afed976b5d271d84c972d5d1b64
ilentiy <i.brykov9@bk.ru>
2022-10-07 06:20:56 +0300

    Initial commit

---
+++ MyStore/MyStore/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MyStore
+//
+//  Created by Илья on 06.10.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
